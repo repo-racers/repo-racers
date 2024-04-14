8a2c156a4d5bb3c76ea119860dbfcb406787caaf
Kevin Ryan <kevinryan1973@gmail.com>
2024-04-14 10:17:01 +0000

    update hoepage hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+import themer from "@tailus/themer";
 module.exports = {
   content: [
     './**/*.html',
     // Add paths to all other files that use Tailwind CSS classes here
+  ],
+  plugins: [
+    themer({
+        palette: {
+            extend : "nature"
+        },
+        radius: "smoothest",
+        background: "light",
+        border: "light",
+        padding:"large",
+        components: {
+            button: {
+                rounded : "2xl"
+            }
+        }
+    })
   ],
   corePlugins: {
       preflight: false,
